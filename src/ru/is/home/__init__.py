78e8974971e29ad1f9dfa1feeb9f1828bbd6323d
Jon Atli <thasnowbear@yahoo.dk>
2013-10-10 20:17:37 +0000

    Added input handle
    Added input handle
    If "lesa" is second word it goes into read method
    else if "skrifa" it goes into write method
    else pings server with 4

--- src/ru/is/home/__init__.py
+++ src/ru/is/home/__init__.py
@@ -1,1 +1,30 @@
-print('hello World')
+import socket
+import sys
+import struct
+import string
+        
+#useless later Input1.  
+UDP_IP = "127.0.0.1"
+#Given
+UDP_PORT = 69
+
+var = input("Enter something: ")
+#Input1 = servername
+#Input2 = Read or Write
+#Input3 = Document Name
+input1,input2,input3 = var.split(" ")
+
+if input2 == "lesa":
+    #implement Reading
+    print('READING')
+elif input2 == "skrifa":
+    #implement Writing
+    print('WRITING')
+    
+else:
+    #ATM only for connecting
+    sock = socket.socket(socket.AF_INET,
+                        socket.SOCK_DGRAM)
+    s = struct.pack("!H", 4)
+    sock.sendto(s,(UDP_IP,UDP_PORT))
+    sock.close()
