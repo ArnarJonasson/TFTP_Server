2df423eb9c9cdb5b6cccde66394a58cc82a8c44b
Jon Atli <thasnowbear@yahoo.dk>
2013-10-13 22:13:16 +0000

    added error handling for write
    if you get error message from packet ie. requesting a file that does not
    exist
    also try to connect to something random. like aefea
    still need to implement timeout from random ip addresses

--- src/ru/is/home/__init__.py
+++ src/ru/is/home/__init__.py
@@ -6,8 +6,10 @@
 #             4     Acknowledgment (ACK)
 #             5     Error (ERROR)
 
-import socket, sys, struct, string
+import socket, sys, struct, string, array
 
+ErrorMsg = ['Not defined, see error message (if any).', 'File not found.', 'Access violation.', 'Disk full or allocation exceeded.' , 'Illegal TFTP Operation.', 'Illegal TFTP operation.', 'Unknown transfer ID.', 'File already exists.', 'No such user.', 'Failed to negotiate options']
+ErrorCode = array.array('i');
 host = sys.argv[1]
 transfermode = sys.argv[2]
 filename = sys.argv[3]
@@ -28,34 +30,50 @@ def request (trmode, filenm):
     format = "!H%ds" % (len(filenm)+1)
     format += "%ds" % (len(mode)+1)
     s = struct.pack(format, trmode , filenm, mode)
-    sock.sendto(s,(host,69))
-    data, svar = sock.recvfrom(1024)
-    port = svar[1]
-    if trmode == 1:
-        read(port, data)
-    elif trmode == 2:
-        write(port, data)
+    try:
+        sock.sendto(s,(host,69))
+        data, svar = sock.recvfrom(1024)
+        port = svar[1]
+        if trmode == 1:
+            read(port, data)
+        elif trmode == 2:
+            write(port, data)
+    except:
+        print("Cannot connect to " + host)
+        
+def checkPackage(package):
+    pack = struct.unpack("!HH", package[:4])
+    if(pack[0] == 5):
+        ErrorCode.insert(0, pack[0])
+        ErrorCode.insert(1, pack[1])
+        return False
+    else:
+        return True
 
 def read(prt, dta):
     nextblock = 1
     strong = dta[4:]
-    if len(dta) == 516:
-        Receiving = True;
-    #While loop to recvieve all packages.
-    while Receiving:
-        #if last package was just received - sending last ACK pack.
-        if len(dta) != 516:
-            Receiving = False;
-            ack = struct.pack("!HH", 4 , nextblock)
-            sock.sendto(ack,(host,prt))
-            writeToFile(strong)
-        #receiving packages.
-        else:
-            ack = struct.pack("!HH", 4 , nextblock)
-            sock.sendto(ack,(host,prt))
-            dta, svar = sock.recvfrom(1024)
-            strong += dta [4:]
-            nextblock += 1
+    if checkPackage(dta) == False:
+        print ErrorMsg[ErrorCode.index(1)]
+        #Restart application and ask for new input
+    else:
+        if len(dta) == 516:
+            Receiving = True;
+        #While loop to recvieve all packages.
+        while Receiving:
+            #if last package was just received - sending last ACK pack.
+            if len(dta) != 516:
+                Receiving = False;
+                ack = struct.pack("!HH", 4 , nextblock)
+                sock.sendto(ack,(host,prt))
+                writeToFile(strong)
+            #receiving packages.
+            else:
+                ack = struct.pack("!HH", 4 , nextblock)
+                sock.sendto(ack,(host,prt))
+                dta, svar = sock.recvfrom(1024)
+                strong += dta [4:]
+                nextblock += 1  
             
 def write(dta, prt):
     #TODO Implement
