23e132fde6a21d86f178666e544fe38dce1fddf4
ArnarJ <arnarjonasson@gmail.com>
2013-10-13 22:57:09 +0200

    Búinn að brjóta upp í föll.

--- src/ru/is/home/__init__.py
+++ src/ru/is/home/__init__.py
@@ -1,38 +1,25 @@
-import socket
-import sys
-import struct
-import string
 
+#           opcode  operation
+#             1     Read request (RRQ)
+#             2     Write request (WRQ)
+#             3     Data (DATA)
+#             4     Acknowledgment (ACK)
+#             5     Error (ERROR)
 
-Receiving = False;
-#useless later Input1.  
-UDP_IP = ""
-#Given
-UDP_PORT = 69
+import socket, sys, struct, string
 
-var = raw_input("Enter something: ")
-#Input1 = servername
-#Input2 = Read or Write
-#Input3 = Document Name
-input1,input2,input3 = var.split(" ")
-UDP_IP = input1
-filename = input3
+host = sys.argv[1]
+transfermode = sys.argv[2]
+filename = sys.argv[3]
+#UDP_PORT = sys.argv[4]
 
-if input2 == "lesa":
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    #sock.connect((UDP_IP, UDP_PORT))
-    mode = "octet"
-    format = "!H%ds" % (len(filename)+1)
-    format += "%ds" % (len(mode)+1)
-    #packing read request
-    s = struct.pack(format, 1, filename, mode)
-    #sending read request
-    sock.sendto(s,(UDP_IP,69))
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+mode = "octet"
+
+def read ():
     nextblock = 1
     lastpkt = s
-    data, svar = sock.recvfrom(1024)
-    strong = ""
-    strong = data[4:]
+    
     if len(data) == 516:
         Receiving = True;
     #While loop to recvieve all packages.
@@ -49,7 +36,8 @@ if input2 == "lesa":
             data, svar = sock.recvfrom(1024)
             strong += data [4:]
             nextblock += 1
-            
+
+def write ():
     #CREATE FILE AND WRITE TO IT.
     fo = open(filename, "wb")
     fo.write(strong)
@@ -57,10 +45,24 @@ if input2 == "lesa":
     fo.close()
     sock.close()
     print('File Recieved')
-elif input2 == "skrifa":
-    #implement Writing
-    print('WRITING')
-    
-else:
-    #ERROR EXPLAIN SYNTAX
+
+def request (trmode, filenm):
+    format = "!H%ds" % (len(filenm)+1)
+    format += "%ds" % (len(mode)+1)
+    s = struct.pack(format, trmode , filenm, mode)
+    sock.sendto(s,(host,69))
+    data, svar = sock.recvfrom(1024)
+    port = svar[1]
+    if trmode == 1:
+        read(port, filenm)
+    elif trmode == 2:
+        write(port, filenm)
+
+if transfermode == "lesa":
+    request(1, filename)
+elif transfermode == "skrifa":
+    request(2, filename)
+else
     print('Please use Servername Read/Write FileName')
+
+   
